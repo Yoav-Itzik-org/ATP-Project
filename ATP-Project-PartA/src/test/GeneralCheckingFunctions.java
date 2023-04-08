6d9d859f40d17d3a40ad113e0e11d5a71ecc8c21
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2023-04-08 17:44:08 +0300

    Added github link method to GeneralCheckingFunctions class

--- ATP-Project-PartA/src/test/GeneralCheckingFunctions.java
+++ ATP-Project-PartA/src/test/GeneralCheckingFunctions.java
@@ -1,4 +1,8 @@
 package test;
 
 public class GeneralCheckingFunctions {
+    public static String getGithubLink() {
+        String githubLink = "https://github.com/yoavzelinger/ATP-Project";
+        return githubLink;
+    }
 }
