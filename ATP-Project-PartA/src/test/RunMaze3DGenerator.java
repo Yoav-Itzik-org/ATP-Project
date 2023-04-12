149ef991df92147d3a8d92058b1d328afc49f3e1
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2023-04-12 18:28:59 +0300

    Finalizing 3D

--- ATP-Project-PartA/src/test/RunMaze3DGenerator.java
+++ ATP-Project-PartA/src/test/RunMaze3DGenerator.java
@@ -13,13 +13,13 @@ public class RunMaze3DGenerator {
 //        prints the time it takes the algorithm to run
         System.out.printf("Maze generation time(ms): %s%n", mazeGenerator.measureAlgorithmTimeMillis(500, 500/*rows*/,500/*columns*/));
 //         generate another maze
-        Maze3D maze = mazeGenerator.generate(3/*depth*/, 3/*rows*/, 5/*columns*/);
+        Maze3D maze = mazeGenerator.generate(500/*depth*/, 500/*rows*/, 500/*columns*/);
 //         prints the maze
         maze.print();
 //         get the maze entrance
         Position3D startPosition = maze.getStartPosition();
 //         print the start position
-        System.out.printf("Start Position: %s%n", startPosition); // format "{row,column}"
+        System.out.printf("Start Position: %s%n", startPosition); // format "{depth,row,column}"
 //         prints the maze exit position
         System.out.printf("Goal Position: %s%n", maze.getGoalPosition());
     }
