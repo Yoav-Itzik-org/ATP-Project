563443f5052cfdef5016d028c1a5bb693aa85784
Yoav Zelinger <yoavzel@post.bgu.ac.il>
2023-05-16 19:38:53 +0300

    Finished simple compressions (and decompression)

---
+++ ATP-Project-PartB/src/IO/SimpleCompressorOutputStream.java
@@ -0,0 +1,42 @@
+package IO;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class SimpleCompressorOutputStream extends OutputStream {
+    OutputStream out;
+    public SimpleCompressorOutputStream(OutputStream out){
+        this.out = out;
+    }
+    public void write(int b) throws IOException{out.write(b);}
+    public void write(byte[] b)throws IOException{
+        if(b == null)
+            return;
+        int sequenceLength = 0;
+        byte previousByte = b[8];
+        int byteIndex = 0;
+        for(; byteIndex < 8; byteIndex++)
+            write(b[byteIndex]);
+        for(; byteIndex < b.length; byteIndex++){
+            byte currentByte = b[byteIndex];
+            if(currentByte != previousByte){
+                splitAndWrite(sequenceLength, previousByte);
+                previousByte = currentByte;
+                sequenceLength = 1;
+            }
+            else
+                sequenceLength++;
+        }
+        splitAndWrite(sequenceLength, previousByte);
+    }
+    public void splitAndWrite(int amount, byte currentByte)throws IOException{
+        while (amount > 255){
+            write(currentByte);
+            write(255);
+            write((currentByte + 1) % 2);
+            write(0);
+            amount -= 255;
+        }
+        write(currentByte);
+        write(amount);
+    }
+}
---
+++ ATP-Project-PartB/src/IO/SimpleDecompressorInputStream.java
@@ -0,0 +1,35 @@
+package IO;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SimpleDecompressorInputStream extends InputStream {
+    InputStream in;
+    public SimpleDecompressorInputStream(InputStream in){this.in = in;}
+    public int read() throws IOException {return in.read();}
+    public int read(byte[] b)throws IOException{
+        if(b == null)
+            return -1;
+        int bIndex = 0;
+        for(; bIndex < 8 ; bIndex++) {
+            if (in.available() == 0)
+                return -1;
+            b[bIndex] = (byte) in.read();
+        }
+        if(in.available() % 2 == 1)
+            return -1;
+        while (in.available() > 0){
+            byte currentByte = (byte) in.read();
+            byte sequenceLength = (byte) in.read();
+            if(bIndex + sequenceLength >= b.length)
+                return -1;
+            insertSequence(b, bIndex, sequenceLength, currentByte);
+            bIndex += sequenceLength;
+        }
+        return 1;
+    }
+    public void insertSequence(byte[] b, int offsetIndex, byte sequenceLength, byte currentByte){
+        for (int bIndex = 0; bIndex < sequenceLength; bIndex++)
+            b[offsetIndex + bIndex] = currentByte;
+    }
+}
