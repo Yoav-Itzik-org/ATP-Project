14b892b7481733a6f271cbf8fa64ea9cebb1c682
Itzak Elbazis <elbazis@post.bgu.ac.il>
2023-06-18 11:52:04 +0300

    About section

--- ATP-Project-PartC/src/View/About.java
+++ ATP-Project-PartC/src/View/About.java
@@ -3,14 +3,11 @@ package View;
 import javafx.application.Platform;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import static javax.swing.text.html.CSS.Attribute.PADDING;
 
 public class About implements Initializable {
     public javafx.scene.control.Button exit;
@@ -27,13 +24,10 @@ public class About implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         text.setWrapText(true);
-        text.setText("Hello and Welcome to Itzik and Yoav's project!\n" +
-                "This application allows you to generate and solve mazes using various algorithms. " +
-                "Create random mazes of different sizes and complexities, and challenge yourself by finding the optimal " +
-                "path from the entrance to the exit. Enjoy the visually appealing representation of mazes and interact " +
-                "with them using keyboard or mouse controls. Whether you're a puzzle enthusiast, algorithm lover, " +
-                "or simply looking for a fun activity, this application provides an immersive experience in the intriguing " +
-                "world of mazes. Get ready to explore and solve mazes like never before!\n"
+        text.setText("""
+                Hello and Welcome to Itzik and Yoav's project!
+                This application allows you to generate and solve mazes using various algorithms. Create random mazes of different sizes and complexities, and challenge yourself by finding the optimal path from the entrance to the exit. Enjoy the visually appealing representation of mazes and interact with them using keyboard or mouse controls. Whether you're a puzzle enthusiast, algorithm lover, or simply looking for a fun activity, this application provides an immersive experience in the intriguing world of mazes. Get ready to explore and solve mazes like never before!
+                """
         );
 
         Image SmallImageNearExit = null;
